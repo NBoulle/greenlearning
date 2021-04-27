dc929740fafb884466cb9577e326448d244d867a
Nicolas Boulle <nicolas.boulle@maths.ox.ac.uk>
2021-04-27 11:44:07 +0100

    update Schrodinger dataset

--- examples/schrodinger.m
+++ examples/schrodinger.m
@@ -5,12 +5,11 @@ function output_example = schrodinger()
 dom = [-3,3];
 
 % Parameter of the equation
-%V = chebfun(@(x)2-(x>=-1).*(x<=1),dom,'splitting','on')
-%V = chebfun(@(x) x^2, dom); % Square potential
-V = chebfun(@(x) x^2+1.5*exp(-(x/0.25)^4), dom); % Double well potential
+h = 0.1;
+V = chebfun(@(x) x^2+1.5*exp(-(x/0.25)^4), dom);
 
 % Differential operator
-N = chebop(@(x,u)-0.1*diff(u,2) + V(x)*u, dom);
+N = chebop(@(x,u)-h^2*diff(u,2) + V(x)*u, dom);
 
 % Boundary conditions
 N.bc = @(x,u) [u(dom(1)); u(dom(2))];
