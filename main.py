2030a4efbdacc7c29b1342c917119cc208e3f12a
Nicolas Boulle <nicolas.boulle@maths.ox.ac.uk>
2021-05-03 10:54:22 +0100

    update main file

--- main.py
+++ main.py
@@ -1,30 +1,22 @@
 import greenlearning as gl
 
-#from evaluate_complex_hom import evaluate_complex
-
-#from greenlearning.utils.backend import tf
-#tf.set_random_seed(101)
-
 def main():
     
     # Construct neural networks for G and homogeneous solution
-    G_network = gl.matrix_networks([4] + [50] * 4 + [1], "rational", (1,1))
-    U_hom_network = gl.matrix_networks([2] + [50] * 4 + [1], "rational", (1,))
+    G_network = gl.matrix_networks([2] + [50] * 4 + [1], "rational", (1,1))
+    U_hom_network = gl.matrix_networks([1] + [50] * 4 + [1], "rational", (1,))
     
     # Define the model
     model = gl.Model(G_network, U_hom_network)
     
     # Train the model on the selected dataset
-    model.train("examples_2d/datasets/","poisson_disk")
+    model.train("examples/datasets/","helmholtz")
     
     # Plot the results
-    #model.plot_results()
+    model.plot_results()
     
-    # Save the training loss
-    #model.save_loss()
-    
-    #model.save_results()
-    #evaluate_complex(model.sess, model.idn_N_pred, model.x, model.U_hom)
+    # Save the results
+    model.save_results()
     
     # Close the session
     model.sess.close()
